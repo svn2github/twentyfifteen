c5bf54930470adf5cc2d49fd8a6c8b4e60678177
lance <lance@d957f892-c61d-0410-b221-f235e6eecf30>
2014-11-20 17:12:43 +0000

    Twenty Fifteen: merge with latest core changes up to [wp30479].
    git-svn-id: https://wpcom-themes.svn.automattic.com/twentyfifteen@21755 d957f892-c61d-0410-b221-f235e6eecf30

--- inc/customizer.php
+++ inc/customizer.php
@@ -28,8 +28,8 @@ function twentyfifteen_customize_register( $wp_customize ) {
 	) );
 
 	$wp_customize->add_setting( 'color_scheme_css', array(
-		'default'           => '',
-		'transport'         => 'postMessage',
+		'default'   => '',
+		'transport' => 'postMessage',
 	) );
 
 	$wp_customize->add_control( 'color_scheme', array(
