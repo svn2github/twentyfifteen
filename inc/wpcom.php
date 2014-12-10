6081f35881a674010c14f2becd9e3b8b3d220df0
iamtakashi <iamtakashi@d957f892-c61d-0410-b221-f235e6eecf30>
2014-12-10 20:32:22 +0000

    Twenty Fifteen: Remove the switch statement since we don't have the bundled color schemes in WP.com version.
    git-svn-id: https://wpcom-themes.svn.automattic.com/twentyfifteen@22139 d957f892-c61d-0410-b221-f235e6eecf30

--- inc/wpcom.php
+++ inc/wpcom.php
@@ -13,66 +13,16 @@
  */
 function twentyfifteen_wpcom_setup() {
 	global $themecolors;
-	$color_scheme_option = get_theme_mod( 'color_scheme', 'default' );
 
 	// Set theme colors for third party services.
 	if ( ! isset( $themecolors ) ) {
-
-		switch( $color_scheme_option ) {
-			case 'dark' :
-				$themecolors = array(
-					'bg'     => '202020',
-					'border' => '2f2f2f',
-					'text'   => 'bebebe',
-					'link'   => 'bebebe',
-					'url'    => 'bebebe',
-				);
-				break;
-			case 'yellow' :
-				$themecolors = array(
-					'bg'     => 'ffffff',
-					'border' => 'e6e6e6',
-					'text'   => '111111',
-					'link'   => '111111',
-					'url'    => '111111',
-				);
-				break;
-			case 'pink' :
-				$themecolors = array(
-					'bg'     => 'ffffff',
-					'border' => 'eae8e6',
-					'text'   => '352712',
-					'link'   => '352712',
-					'url'    => '352712',
-				);
-				break;
-			case 'purple' :
-				$themecolors = array(
-					'bg'     => 'ffffff',
-					'border' => 'e9e8ed',
-					'text'   => '2e2256',
-					'link'   => '2e2256',
-					'url'    => '2e2256',
-				);
-				break;
-			case 'blue' :
-				$themecolors = array(
-					'bg'     => 'ffffff',
-					'border' => 'e9e8eb',
-					'text'   => '22313f',
-					'link'   => '22313f',
-					'url'    => '22313f',
-				);
-				break;
-			default :
-				$themecolors = array(
-					'bg'     => 'ffffff',
-					'border' => 'eaeaea',
-					'text'   => '333333',
-					'link'   => '333333',
-					'url'    => '333333',
-				);
-		}
+		$themecolors = array(
+			'bg'     => 'ffffff',
+			'border' => 'eaeaea',
+			'text'   => '333333',
+			'link'   => '333333',
+			'url'    => '333333',
+		);
 	}
 }
 add_action( 'after_setup_theme', 'twentyfifteen_wpcom_setup' );
